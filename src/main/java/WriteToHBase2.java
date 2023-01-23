7f359ffc4036a117c696dd158abfd528afd04efb
Omar Guermazi <guermaziomar1@gmail.com>
2023-01-23 15:21:17 +0100

    WritingToHBase

--- src/main/java/WriteToHBase2.java
+++ src/main/java/WriteToHBase2.java
@@ -1,5 +1,7 @@
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.fs.Path;
@@ -8,20 +10,11 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
 import org.apache.hadoop.hbase.mapreduce.TableReducer;
 import java.io.IOException;
-
-import javax.print.attribute.standard.Destination;
-
-import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.hadoop.hbase.client.Admin;
-import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.HColumnDescriptor;
 
-public class WriteToHBase {
+public class WriteToHBase2 {
     private static final String TABLE_NAME = "oguermazi:citytraffic_table";
     static String[] destinations; //IL FAUT CHANGER LE NAMESPACE
 
@@ -45,14 +38,14 @@ public class WriteToHBase {
                     for(int i=0; i<line.length ; i++){
                         put.addColumn(Bytes.toBytes("day_stats"), Bytes.toBytes(destinations[i]), line[i].getBytes());
                     }
-                    
+
                     context.write(new Text(key.toString()), put);
                 }
             }
         }
     }
 
-    public static void createOrOverwrite(Admin admin, HTableDescriptor table) throws IOException {
+    public static void createOrOverwrite(Admin admin, TableDescriptor table) throws IOException {
         if (admin.tableExists(table.getTableName())) {
             admin.disableTable(table.getTableName());
             admin.deleteTable(table.getTableName());
@@ -62,9 +55,10 @@ public class WriteToHBase {
     public static void createTable(Connection connect) {
         try {
             final Admin admin = connect.getAdmin();
-            HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf(TABLE_NAME));
-            HColumnDescriptor fam = new HColumnDescriptor(Bytes.toBytes("day_stats"));
-            tableDescriptor.addFamily(fam);
+            TableDescriptorBuilder tableDescriptorBuilder = TableDescriptorBuilder.newBuilder(TableName.valueOf(TABLE_NAME));
+            ColumnFamilyDescriptorBuilder fam = ColumnFamilyDescriptorBuilder.newBuilder(Bytes.toBytes("day_stats"));
+            tableDescriptorBuilder.setColumnFamily(fam.build());
+            TableDescriptor tableDescriptor = tableDescriptorBuilder.build();
             createOrOverwrite(admin, tableDescriptor);
             admin.close();
         } catch (Exception e) {
@@ -74,14 +68,14 @@ public class WriteToHBase {
     }
 
     public static void main (String[] args) throws Exception {
-        Configuration conf = new HBaseConfiguration();
+        Configuration conf = HBaseConfiguration.create();
         Job job = Job.getInstance(conf, "Write to HBase example");
         job.setJarByClass(WriteToHBase.class);
         //create the table (sequential part)
         Connection connection = ConnectionFactory.createConnection(conf);
         createTable(connection);
         //input from HDFS file
-        FileInputFormat.addInputPath(job, new Path("/user/hgassara/hour_reducer_data/part-r-00000"));
+        FileInputFormat.addInputPath(job, new Path("/user/oguermazi/hour_reducer_data/part-r-00000"));
         job.setInputFormatClass(TextInputFormat.class);
         job.setMapOutputKeyClass(LongWritable.class);
         job.setMapOutputValueClass(Text.class);
